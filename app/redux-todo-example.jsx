ce5f37d161e7c7daf74efc5a33a574f5068753b9
Clint Bush <clint.bush@homesite.com>
2017-02-28 16:17:53 -0800

    Add subscribe and dev tools

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -21,7 +21,16 @@ const reducer = (state: object = stateDefault, action: object) => {
     return state
 }
 
-let store = redux.createStore(reducer)
+let store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+// subscribe to changes
+let unsubscribe = store.subscribe(() => {
+    // subcribe() returns a function to unsubscribe
+    let state = store.getState()
+    console.log('SearchText is', state.searchText)
+})
 
 let currentState = store.getState()
 console.log('currentState', currentState)
@@ -31,4 +40,14 @@ store.dispatch({
     searchText: 'some search text'
 })
 
-console.log('newState', store.getState())
+store.dispatch({
+    type: 'CHANGE_SEARCHTEXT',
+    searchText: 'different search terms'
+})
+
+unsubscribe()
+
+store.dispatch({
+    type: 'CHANGE_SEARCHTEXT',
+    searchText: 'more things to search'
+})
